185fe0b49d2df0eb241fc208f38f53613afafb07
YauheniMel <207melnik@gmail.com>
2022-08-11 21:40:01 +0300

    ref: Update form style

--- src/conponents/FormField/FormField.jsx
+++ src/conponents/FormField/FormField.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import classes from './FormField.module.css';
 
 class FormField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+  }
+
+  handleInputFocus() {
+    this.inputRef.current.focus();
+  }
+
   render() {
     return (
       <div className={classes.wrap}>
         <label
-          className={`${classes.label}`}
+          className={classes.label}
+          onClick={this.handleInputFocus}
         >
           {this.props.label}
         </label>
@@ -14,18 +25,31 @@ class FormField extends React.Component {
           this.props.type === 'textarea'
             ? (
               <textarea
+                ref={this.inputRef}
                 className={classes.input}
                 rows="7"
                 placeholder={this.props.placeholder}
               />
             ) : (
               <input
+                ref={this.inputRef}
                 className={classes.input}
-                type={this.props.type}
+                type={this.props.type === 'date' || this.props.type}
+                onFocus={() => {
+                  if(this.props.type === 'date') {
+                    this.inputRef.current.type = 'date'
+                  }
+                }}
+                onBlur={() => {
+                  if(this.props.type === 'date' && !this.inputRef.current.value) {
+                    this.inputRef.current.type = 'text'
+                  }
+                }}
                 placeholder={this.props.placeholder}
               />
             )
         }
+        <span>{this.props.label}</span>
       </div>
     )
   }
